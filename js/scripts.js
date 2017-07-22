ca9e5f011872da22161c623e3d161020081b20b1
Perrin Levy <perrin@work.coach>
2017-07-22 10:50:18 -0500

    loader

--- js/scripts.js
+++ js/scripts.js
@@ -159,12 +159,12 @@ $(window).scroll(function(){
 
 	// Append .background-image-holder <img>'s as CSS backgrounds
 
-	$('.background-image-holder').each(function(){
-		var imgSrc= $(this).children('img').attr('src');
-		$(this).css('background', 'url("' + imgSrc + '")');
-    	$(this).children('img').hide();
-        $(this).css('background-position', '50% 0%');
-	});
+	//$('.background-image-holder').each(function(){
+	//	var imgSrc= $(this).children('img').attr('src');
+	//	$(this).css('background', 'url("' + imgSrc + '")');
+//    	$(this).children('img').hide();
+//        $(this).css('background-position', '50% 0%');
+//	});
 
 	// Accordion
 
